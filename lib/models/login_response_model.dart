a4629444a0f99ec4b79c18e8f812a7a10c31e9de
Berke Çınar <brkcnr.16@gmail.com>
2024-01-29 15:44:37 +0300

    parameter name changed

--- lib/models/login_response_model.dart
+++ lib/models/login_response_model.dart
@@ -1,7 +1,7 @@
 import 'login_response_parameters_model.dart';
 
 class LoginResponse {
-  LoginResponse({required this.authValues, required this.statusCode});
-  final LoginResponseParameters authValues;
+  LoginResponse({required this.loginResponseParameters, required this.statusCode});
+  final LoginResponseParameters loginResponseParameters;
   final int statusCode;
 }
