f480192cbeb1005de7c5e4ce14aa099759bd83f1
Berke Çınar <brkcnr.16@gmail.com>
2024-01-29 20:12:45 +0300

    login web service created

---
+++ lib/services/login_web_service.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+
+import '../models/login_request_arguments_model.dart';
+import '../models/login_response_model.dart';
+import 'package:http/http.dart';
+
+import '../models/login_response_parameters_model.dart';
+
+class LoginWebService {
+  late LoginResponseParameters loginResponseParameters = LoginResponseParameters(
+    token: '',
+  );
+
+  Future<LoginResponse> sendLoginRequest(LoginRequestArguments loginRequestArguments) async {
+    print('response');
+
+    dynamic header = {
+      "content-type": "application/json"
+    };
+
+    Map bodyObject = {
+      "email": loginRequestArguments.email,
+      "password": loginRequestArguments.password
+      //'token': 'my_token',
+    };
+
+    var body = json.encode(bodyObject);
+
+    var uri = Uri.parse('https://reqres.in/api/login');
+
+    var response = await post(uri, headers: header, body: body);
+
+    loginResponseParameters = LoginResponseParameters.fromJson(
+      jsonDecode(response.body),
+    );
+
+    // return response.statusCode;
+    return LoginResponse(
+      loginResponseParameters: loginResponseParameters,
+      statusCode: response.statusCode,
+    );
+  }
+
+}
