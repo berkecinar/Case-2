2a7364a67481adbfd1149547375ea94f65180c5c
Berke Çınar <brkcnr.16@gmail.com>
2024-01-29 15:32:01 +0300

    local storage controller created. Getter/setter and delete methods implemented for userToken and isLogged in parameter which stored in SharedPreferences

---
+++ lib/controllers/local_storage_controller.dart
@@ -0,0 +1,69 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../models/login_response_model.dart';
+
+// Constant SharedPereferences Key's
+const isLoggedInKey = 'isLoggedIn';
+const userTokenKey = 'userToken';
+
+//Sharedpreferences Getter
+final sharedPreferencesProvider = Provider((_) async {
+  return await SharedPreferences.getInstance();
+});
+
+//Provider must watched by every methods which update state
+final setLoginStateProvider = StateProvider<LoginResponse?>(
+  (ref) => null,
+);
+
+
+//isLogged Getter/Setter
+final getIsLoggedInProvider = FutureProvider<bool>(
+      (ref) async {
+    final prefs = await ref.watch(sharedPreferencesProvider);
+    ref.watch(setLoginStateProvider);
+    return prefs.getBool(isLoggedInKey) ?? false;
+  },
+);
+
+final setIsLoggedInProvider = StateProvider.family<void, bool>(
+  (ref, isLoggedIn) async {
+    final prefs = await ref.watch(sharedPreferencesProvider);
+    ref.watch(setLoginStateProvider);
+    prefs.setBool(
+      isLoggedInKey,
+      isLoggedIn,
+    );
+  },
+);
+
+
+//userToken Getter/Setter
+final setUserTokenProvider = StateProvider.family<void, String>(
+  (ref, userToken) async {
+    final prefs = await ref.watch(sharedPreferencesProvider);
+    ref.watch(setLoginStateProvider);
+    prefs.setString(
+      userTokenKey,
+      userToken,
+    );
+  },
+);
+
+final getUserTokenProvider = FutureProvider<String>(
+  (ref) async {
+    final prefs = await ref.watch(sharedPreferencesProvider);
+    ref.watch(setLoginStateProvider);
+    return prefs.getString(userTokenKey) ?? '';
+  },
+);
+
+
+final clearStorage = StateProvider<dynamic>(
+  (ref) async {
+    final prefs = await ref.watch(sharedPreferencesProvider);
+    final isCleared = await prefs.clear();
+    return isCleared;
+  },
+);
