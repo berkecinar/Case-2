d7ae0d8a339d090b31118cc7869e4e280b59537e
Berke Çınar <brkcnr.16@gmail.com>
2024-01-29 15:29:35 +0300

    riverpod providers, references and navigators added to main and CaseTwoApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,46 @@
+import 'package:case_2/views/home_page_view.dart';
 import 'package:case_2/views/login_page_view.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'controllers/local_storage_controller.dart';
 
 void main() {
-  runApp(const CaseTwoApp());
+  runApp(
+    const ProviderScope(
+      child: CaseTwoApp(),
+    ),
+  );
 }
 
-class CaseTwoApp extends StatelessWidget {
+class CaseTwoApp extends HookConsumerWidget  {
   const CaseTwoApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       title: 'Case Two',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginPageView(),
+      home: ref.watch(getIsLoggedInProvider).when(
+            data: (bool isAuthenticated) =>
+                isAuthenticated ? const HomePageView() : const LoginPageView(),
+            loading: () {
+              return const Scaffold(
+                body: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            },
+            error: (error, stacktrace) => const LoginPageView(),
+          ),
+      routes: {
+        "Home": (context) => const HomePageView(),
+        "Login": (context) => const LoginPageView(),
+      },
     );
   }
 }
-
-
